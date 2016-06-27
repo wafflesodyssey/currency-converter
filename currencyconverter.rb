111f0af1d339053b4431199b8154d632d8f40dea
tony un <tony un>
2016-06-27 06:44:56 -0400

    Second commit. Added class and function.

--- currencyconverter.rb
+++ currencyconverter.rb
@@ -1,1 +1,15 @@
-#start, two classes
+#start, converting currencies
+
+#0.021 PHP to USD
+PHP = 0.021
+USD = 1.00
+
+
+class Money
+  currency_converter.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD)
+
+
+def PHP (money, value)
+  @money
+  @value
+end
